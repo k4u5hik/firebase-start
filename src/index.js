33e3ec61c7e5b861f496cdb0da4895b43899df9e
Kaushik Chemburkar <attherate@gmail.com>
2022-11-19 17:24:48 +1100

    Basic Setup Instructions: - package.json created with {} inside it - webpack.config.js created with basic setup code - index.html in dist folder - index.js in src folder - installed the required software as in package.json - main.js is created by the webpack - npx webpack --watch - npx serve dist - if it all works without errors check console.log from the browser inspector. - DONT FORGET .gitignore file

---
+++ src/index.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB-aIjJXM_FPRCjfVmHcjaKuU9XgvI2n3Q",
+  authDomain: "verycutebunhouse.firebaseapp.com",
+  projectId: "verycutebunhouse",
+  storageBucket: "verycutebunhouse.appspot.com",
+  messagingSenderId: "295458883913",
+  appId: "1:295458883913:web:ddfe99921bf3e1092b3298",
+  measurementId: "G-RT75DTD277"
+};
+
+console.log ('Hello from Firebase');
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
